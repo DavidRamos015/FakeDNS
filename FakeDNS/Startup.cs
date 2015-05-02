edc323edd280d461475107aca35ce9cb9f24f64e
David Ramos <davidramos015@gmail.com>
2015-05-01 22:26:12 -0700

    test

---
+++ FakeDNS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FakeDNS.Startup))]
+namespace FakeDNS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
